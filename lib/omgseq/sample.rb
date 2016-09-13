de788c20a1a087e8b1cad767897894e44e346ede
tazro inutano ohta <inutano@gmail.com>
2016-09-13 16:03:38 +0900

    updated to follow guideline

--- lib/omgseq/sample.rb
+++ lib/omgseq/sample.rb
@@ -53,24 +53,98 @@ module OMGSeq
       biosample_header.map{|field| object[field.to_sym] }
     end
 
+    def bs_date(entry)
+      date = entry[7]
+      if date && !date.empty?
+        DateTime.parse(entry[7]).strftime("%Y-%m-%dT%H:%M:%S")
+      else
+        "2015"
+      end
+    end
+
+    def bs_location(entry)
+      location = entry[11]
+      if location && !location.empty?
+        "Japan:" + entry[11].split(", ").reverse.join(", ")
+      else
+        "Japan"
+      end
+    end
+
+    def bs_latlon(entry)
+      latlon = entry[8]
+      if latlon && !latlon.empty?
+        ll = latlon.gsub(/\s/,"").split(",").map{|v| v.to_f }
+        n = "%0.4f" % ll[0]
+        e = "%0.4f" % ll[1]
+        "#{n} N #{e} E"
+      else
+        "NA"
+      end
+    end
+
+    def bs_elev(entry)
+      latlon = entry[8]
+      if latlon && !latlon.empty?
+        ll = latlon.gsub(/\s/,"").split(",").map{|v| v.to_f }
+        if ll[2]
+          ("%0.2f" % ll[2]).to_s + "m"
+        end
+      end
+    end
+
+    def bs_air_temp_regm(entry)
+      air_temp_regm = entry[17]
+      if air_temp_regm && !air_temp_regm.empty?
+        ("%0.1f" % air_temp_regm.to_f).to_s + "˚C"
+      else
+        ""
+      end
+    end
+
+    def bs_rainfall_regm(entry)
+      rainfall_regm = entry[18]
+      if rainfall_regm && !rainfall_regm.empty?
+        ("%0.1f" % rainfall_regm.to_f).to_s + "mm"
+      else
+        ""
+      end
+    end
+
+    def bs_radiation_regm(entry)
+      radiation_regm = entry[19]
+      if radiation_regm && !radiation_regm.empty?
+        "Sunlight:" + ("%0.1f" % radiation_regm.to_f).to_s + "MJ/m^2"
+      else
+        ""
+      end
+    end
+
+    def bs_humidity_regm(entry)
+      humidity_regm = entry[23]
+      if humidity_regm && !humidity_regm.empty?
+        ("%0.1f" % humidity_regm.to_f).to_s + "%"
+      else
+        ""
+      end
+    end
+
+    def bs_days_since_blooming(entry)
+      dsb = entry[13]
+      if dsb && !dsb.empty?
+        "Days_since_blooming: #{dsb}days"
+      end
+    end
+
     def create_biosample_object(entry)
-      label    = entry[1].split("_")[0]
-      date     = entry[7] ? DateTime.parse(entry[7]).strftime("%Y/%m/%d") : "2015"
-      latlon   = entry[8] || "NA"
-      location = entry[11] || "Japan"
-
-      air_temp_regm  = entry[17] ? entry[17] + "˚C" : ""
-      rainfall_regm  = entry[18] ? entry[18] + "mm" : ""
-      radiation_regm = entry[19] ? entry[19] + "MJ/m^2" : ""
-      humidity_regm  = entry[23] ? entry[23] + "%" : ""
-
+      label = entry[1].split("_")[0]
       {
         # required - data
         sample_name: "omgseq_" + label,
-        sample_title: "16S Sequencing of microbiome on flower of genus Prunus in " + location + ", label number " + label,
-        collection_date: date,
-        geo_loc_name: location,
-        lat_lon: latlon,
+        sample_title: "16S Sequencing of microbiome on flower of genus Prunus#{entry[11] ? " in " + entry[11] : "" }, label number " + label,
+        collection_date: bs_date(entry),
+        geo_loc_name: bs_location(entry),
+        lat_lon: bs_latlon(entry),
 
         # required - fixed
         organism: "flower metagenome",
@@ -83,22 +157,23 @@ module OMGSeq
         plant_body_site: "petal",
 
         # option data
-        elev: "",
-        air_temp_regm: air_temp_regm,
-        rainfall_regm: rainfall_regm,
-        radiation_regm: radiation_regm,
-        humidity_regm: humidity_regm,
+        elev: bs_elev(entry),
+        air_temp_regm: bs_air_temp_regm(entry),
+        rainfall_regm: bs_rainfall_regm(entry),
+        radiation_regm: bs_radiation_regm(entry),
+        humidity_regm: bs_humidity_regm(entry),
+        misc_param: bs_days_since_blooming(entry),
 
         # option fixed
-        bioproject_id: "",
+        host_common_name: "Cherry blossom",
+        host_taxid: "3754",
+        life_stage: "Floweing stage",
+        season_environment: "Spring",
         strain: "unidentified",
         breed: "unidentified",
         cultivar: "unidentified",
-        host_common_name: "Cherry",
-        host_taxid: "3754",
-        life_stage: "Floweing stage",
-        samp_store_temp: "-80˚C",
-        season_environment: "Spring",
+        # bioproject_id: "",
+        # samp_store_temp: "-80˚C",
       }
     end
 
